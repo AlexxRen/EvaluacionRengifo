44030fc833dfdaab9d6e68d4c663bf93a288f399
Alexander <alexren567@outlook.com>
2024-07-01 08:28:27 -0500

    consumir appi

--- EvaluacionRengifo/Views/Consumidor.cs
+++ EvaluacionRengifo/Views/Consumidor.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using EvaluacionRengifo.Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace EvaluacionRengifo.Views
 {
-    internal class Consumidor
+    internal class Consumidor :ApiConsumo
     {
+        public class RestService
+        {
+            HttpClient _client;
+            JsonSerializerOptions _serializerOptions;
+
+            public List<ApiConsumo> Items { get; private set; }
+
+            public RestService()
+            {
+                _client = new HttpClient();
+                _serializerOptions = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+            }
+            public async Task<List<ApiConsumo>> RefreshDataAsync()
+            {
+                Items = new List<ApiConsumo>();
+
+                Uri uri = new Uri(string.Format("https://api.chucknorris.io/jokes/random", string.Empty));
+                try
+                {
+                    HttpResponseMessage response = await _client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        Items = JsonSerializer.Deserialize<List<ApiConsumo>>(content, _serializerOptions);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                }
+
+                return Items;
+            }
+        }
     }
 }
+
+
--- EvaluacionRengifo/Views/VentanaMain.xaml.cs
+++ EvaluacionRengifo/Views/VentanaMain.xaml.cs
@@ -7,7 +7,8 @@ public partial class VentanaMain : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    Consumidor cons = new Consumidor();
+    
         async void OnButtonClicked(object sender, EventArgs args)
         {
             await joke.RelRotateTo(360, 1000);
